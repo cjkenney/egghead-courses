7fb836211aa56a9a3c69bc96a78af16e07e480d0
CJ Kenney <cjkenney.dev@gmail.com>
2019-03-01 14:35:03 -0600

    create initial actions

--- redux-observable/src/components/Stories.js
+++ redux-observable/src/components/Stories.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { loadStories, clear } from '../actions/index';
 
 function mapStateToProps(state) {
   return state;
@@ -8,11 +9,44 @@ function mapStateToProps(state) {
 class Stories extends Component {
   render() {
     return (
-      <pre>
-        <code>{JSON.stringify(this.props, null, 2)}</code>
-      </pre>
+      <div>
+        <button type="button" onClick={this.props.loadStories}>
+          Load Top 3 Stories
+        </button>
+        <button type="button" onClick={this.props.clear}>
+          Clear
+        </button>
+        <StoryList {...this.props} />
+      </div>
     );
   }
 }
 
-export default connect(mapStateToProps)(Stories);
+function StoryList(props) {
+  if (props.items.length === 0) {
+    return null;
+  }
+  return (
+    <div>
+      {props.items.map(item => (
+        <Story {...item} key={item.id} />
+      ))}
+    </div>
+  );
+}
+
+function Story(props) {
+  return <p>{props.title}</p>;
+}
+
+function mapDispatch(dispatch) {
+  return {
+    loadStories: () => dispatch(loadStories()),
+    clear: () => dispatch(clear())
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatch
+)(Stories);
--- redux-observable/src/reducers/index.js
+++ redux-observable/src/reducers/index.js
@@ -1,9 +1,47 @@
+import { LOAD_STORIES, CLEAR_STORIES } from '../actions/index';
+
+const stories = [
+  {
+    by: 'asdfasdf',
+    id: 14967192,
+    title:
+      'Uber and Lyft Said to Offer Drivers a Chance to Participate in I.P.O.s',
+    url:
+      'https://www.nytimes.com/2019/02/28/technology/uber-lyft-drivers-ipos.html'
+  },
+  {
+    by: 'tommy guy',
+    id: 1497222,
+    title: 'Limiting Your Digital Footprints in a Surveillance State',
+    url:
+      'https://www.nytimes.com/2019/02/27/technology/personaltech/digital-footprint-surveillance.html'
+  },
+  {
+    by: 'dude',
+    id: 14967345,
+    title: 'The Discount Cable and Charger Brands You Can Trust',
+    url:
+      'https://www.nytimes.com/2019/02/25/smarter-living/the-discount-cable-and-charger-brands-you-can-trust.html'
+  }
+];
+
 const initialState = {
-  items: []
+  items: [1, 2, 3]
 };
 
 export function storiesReducer(state = initialState, action) {
-  return state;
+  switch (action.type) {
+    case LOAD_STORIES:
+      return {
+        items: stories.slice()
+      };
+    case CLEAR_STORIES:
+      return {
+        items: []
+      };
+    default:
+      return state;
+  }
 }
 
 export default storiesReducer;
